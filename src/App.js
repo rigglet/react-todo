a17b0dd7c91adb61aa653d4278365158288331ee
rigglet <39748939+rigglet@users.noreply.github.com>
2020-08-29 13:22:47 +0100

    Added Form and TodoList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <header>
+      <h1>Neil's Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
